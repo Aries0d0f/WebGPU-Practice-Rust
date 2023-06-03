5766ddfc46c3d848a08d16489cbb3e500371b050
aries0d0f <aries0d0f@gmail.com>
2023-06-03 17:30:34 +0800

    feat: Impl entry.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    println!("Hello, world!");
+use crate::lib::vertex::Vertex;
+use crate::lib::window;
+
+#[path = "lib/mod.rs"]
+mod lib;
+
+pub fn main() {
+    pollster::block_on(window::open(
+        include_str!("shaders/shader.wgsl"),
+        &[
+            Vertex {
+                position: [0.0, 0.5, 0.0, 1.0],
+                color: [1.0, 0.0, 0.0, 1.0],
+            },
+            Vertex {
+                position: [-0.5, -0.5, 0.0, 1.0],
+                color: [0.0, 1.0, 0.0, 1.0],
+            },
+            Vertex {
+                position: [0.5, -0.5, 0.0, 1.0],
+                color: [0.0, 0.0, 1.0, 1.0],
+            },
+        ],
+    ));
 }
