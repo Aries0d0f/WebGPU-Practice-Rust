d81c029d7b75123807f8f4d22aed34213e85e5ab
aries0d0f <aries0d0f@gmail.com>
2023-06-03 17:52:03 +0800

    feat: Draw equilateral triangle.

--- src/main.rs
+++ src/main.rs
@@ -5,19 +5,21 @@ use crate::lib::window;
 mod lib;
 
 pub fn main() {
+    let triangle_height: f32 = (std::f32::consts::PI / 6.0).cos();
+
     pollster::block_on(window::open(
         include_str!("shaders/shader.wgsl"),
         &[
             Vertex {
-                position: [0.0, 0.5, 0.0, 1.0],
+                position: [0.0, 1.0, 0.0, 1.0],
                 color: [1.0, 0.0, 0.0, 1.0],
             },
             Vertex {
-                position: [-0.5, -0.5, 0.0, 1.0],
+                position: [-triangle_height, -0.5, 0.0, 1.0],
                 color: [0.0, 1.0, 0.0, 1.0],
             },
             Vertex {
-                position: [0.5, -0.5, 0.0, 1.0],
+                position: [triangle_height, -0.5, 0.0, 1.0],
                 color: [0.0, 0.0, 1.0, 1.0],
             },
         ],
